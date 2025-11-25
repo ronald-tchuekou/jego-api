f8e98c1b13ac3c2db48f8d0c6e36421e9cae4644
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2025-11-25 18:39:48 +0100

    Add stream endpoint to download file controller

--- app/controllers/download_file_controller.ts
+++ app/controllers/download_file_controller.ts
@@ -1,4 +1,5 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import { createReadStream } from 'node:fs'
 import { sep } from 'node:path'
 
 export default class DownloadFileController {
@@ -6,5 +7,12 @@ export default class DownloadFileController {
     const filePath = request.param('*').join(sep)
     const absolutePath = `storage/${filePath}`
     return response.download(absolutePath, true)
+  }
+  async stream({ response, request }: HttpContext) {
+    const filePath = request.param('*').join(sep)
+    const absolutePath = `storage/${filePath}`
+    return response.stream(createReadStream(absolutePath), (error) => {
+      return [error.message, 400]
+    })
   }
 }
--- build/app/controllers/download_file_controller.js
+++ build/app/controllers/download_file_controller.js
@@ -1,9 +1,17 @@
+import { createReadStream } from 'node:fs';
 import { sep } from 'node:path';
 export default class DownloadFileController {
     async download({ response, request }) {
         const filePath = request.param('*').join(sep);
         const absolutePath = `storage/${filePath}`;
         return response.download(absolutePath, true);
+    }
+    async stream({ response, request }) {
+        const filePath = request.param('*').join(sep);
+        const absolutePath = `storage/${filePath}`;
+        return response.stream(createReadStream(absolutePath), (error) => {
+            return [error.message, 400];
+        });
     }
 }
 //# sourceMappingURL=download_file_controller.js.map
